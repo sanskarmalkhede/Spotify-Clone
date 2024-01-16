9c3b584c8a79f140e9801edf9c08998f0fc07a17
Sanskar M <malkhedesanskar@gmail.com>
2024-01-16 13:56:23 +0530

    Add HomeIcon to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { ReactComponent as HomeIcon } from '../assets/HomeIcon.svg';
 
 export const Sidebar = () => {
   return (
+    <>
     <div>Sidebar</div>
-  )
-}
+    <HomeIcon />  
+    </>
+  );
+};
