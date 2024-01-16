fc4c5b37c93b0698c4cadaa2ecca4907b8e50716
Sanskar M <malkhedesanskar@gmail.com>
2024-01-16 18:41:19 +0530

    Update App.css and Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import './Footer.css'
+
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import { Grid, Slider } from '@mui/material';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <div className="footer">
+        <div className="footer__left">
+          <div className="footer__albumLogo" />
+          <div className="footer__songInfo">
+            <h4>All Me</h4>
+            <p>Drake, Big Sean</p>
+          </div>
+        </div>
+        <div className="footer__center">
+          <ShuffleIcon className="footer__green" />
+          <SkipPreviousIcon className="footer__icon" />
+          <PlayArrowIcon fontSize="large" className="footer__icon" />
+          <SkipNextIcon className="footer__icon" />
+          <RepeatIcon className="footer__green" />
+        </div>
+        <div className="footer__right">
+          <Grid container spacing={2}>
+            <Grid item>
+              <PlaylistPlayIcon />
+            </Grid>
+            <Grid item>
+              <VolumeDownIcon />
+            </Grid>
+            <Grid item xs>
+              <Slider />
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    </>
   )
 }
