37172a03e2a0403c45be0deac126598ff8c0e890
Sanskar M <malkhedesanskar@gmail.com>
2024-01-16 14:05:27 +0530

    Update HomeIcon.svg and Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,8 +4,8 @@ import { ReactComponent as HomeIcon } from '../assets/HomeIcon.svg';
 export const Sidebar = () => {
   return (
     <>
-    <div>Sidebar</div>
-    <HomeIcon />  
+      <div>Sidebar</div>
+      <HomeIcon />
     </>
   );
 };
